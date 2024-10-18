afd8dfbe22180e01c3788d393389a8e88571686b
Dhananjay <dhananjaysable370@gmail.com>
2024-10-18 23:03:07 +0530

    'js'

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 let arr = ['Dhananjay', 7, 33, false, null];
-for (let i = 0; i < arr.length; i++){
-    console.log(arr[i]);
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+class Main{
+    constructor(a,b) {
+        this.a = a;
+        this.b = b;
+    }
 }
+console.log(typeof(Main)) //Function
